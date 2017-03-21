1a5edb20758926e4a17f41b90aa3083837619c62
mclavin <lavin.martin@gmail.com>
2017-03-21 15:12:58 +0100

    added album list to artist

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 /**
  * Created by Martin on 3/18/2017.
+ *
+ * kalla på annan function utanför success instället för att skriva
+ * all kod i success
  */
 var Module = (function(){
     let getArtistAndTracks = () => {
@@ -10,7 +13,19 @@ var Module = (function(){
            success: () => {
                console.log(queryHits);
                let artistId = queryHits.responseJSON.artists.items[0].id;
-               document.getElementById("artistName").innerHTML = queryHits.responseJSON.artists.items[0].name;
+               let artistDiv = document.getElementById("theArtist");
+               artistDiv.innerHTML = "";
+
+               let artistName = document.createElement("h3");
+               let artistNode = document.createTextNode(queryHits.responseJSON.artists.items[0].name);
+               artistName.appendChild(artistNode);
+               artistDiv.appendChild(artistName);
+
+               let img = document.createElement("img");
+               img.setAttribute("class", "artistImage");
+               img.src = queryHits.responseJSON.artists.items[0].images[1].url;
+               artistDiv.appendChild(img);
+
                /**
                 * MOAR GET
                 * @type {string}
@@ -22,13 +37,38 @@ var Module = (function(){
                       let trackList = document.getElementById("artistsTopTracks");
                       trackList.innerHTML = "";
                       for(let i = 0; i < tracks.length; i++){
+                          let trackLink = document.createElement("a");
+                          console.log(tracks[i]);
+                          trackLink.href = tracks[i].uri;
                           let trackLi = document.createElement("li");
                           let trackNode = document.createTextNode(tracks[i].name);
-                          trackLi.appendChild(trackNode);
+                          trackLink.appendChild(trackNode);
+                          trackLi.appendChild(trackLink);
                           trackList.appendChild(trackLi);
                       }
                    }
                });
+
+               let findArtistAlbums = $.ajax({
+                   url: `https://api.spotify.com/v1/artists/${artistId}/albums`,
+                   success: function () {
+                       console.log(findArtistAlbums);
+                       let albumList = document.getElementById("artistsAlbums");
+                       albumList.innerHTML = "";
+                       for(let i = 0; i < findArtistAlbums.responseJSON.items.length; i++){
+                           let albumNode = document.createTextNode(findArtistAlbums.responseJSON.items[i].name);
+                           let albumLi = document.createElement("li");
+                           let albumLink = document.createElement("a");
+                           albumLink.href = findArtistAlbums.responseJSON.items[i].uri;
+
+                           albumLink.appendChild(albumNode);
+                           albumLi.appendChild(albumLink);
+                           albumList.appendChild(albumLi);
+                       }
+                   }
+               })
+
+
            }
         });
         //let findArtistTopTracks = `https://api.spotify.com/v1/artists/1jBFgqfCh0pz5eNIWFvJGQ/top-tracks?country=SE`;
